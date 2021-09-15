ce920793b2ee988d92023f06b60241e4adf34d83
livalue <dimonawesome@gmail.com>
2021-09-16 00:43:45 +0300

    create main

--- cmd/cube_cli/main.go
+++ cmd/cube_cli/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
 
+	"github.com/DmiAS/cube_cli/internal/app/client"
+	"github.com/DmiAS/cube_cli/internal/app/delivery/iproto"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 4 {
+		log.Fatal("invalid number of args")
+	}
+	host, port := args[0], args[1]
+	token, scope := args[2], args[3]
+
+	cli, _ := iproto.NewClient(host, port)
+	cube := client.NewCubeClient(cli)
+
+	resp, err := cube.Send(token, scope)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cube.PrintResponse(resp)
 }
